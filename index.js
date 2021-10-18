59d8a4e410144e404231d5f52d861e3f36686ca3
Ryan Kotsovolos <rkotsovolos@gmail.com>
2021-10-18 16:03:04 -0700

    commit tree a66aa0042d33f9a3a8ee9a4a0ad953c2726cfe2a

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution in this file!
+// // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+     customerName = 'BOB'
+    
+}
+
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+
+
+
+function overwriteBestCustomer() {
+     return bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'fred';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'maybe fred';
+}
+
+
+
+
+
+
+
+
+
+
+
+
